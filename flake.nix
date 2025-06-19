3c02de8619cc13a285081eb89085d0d96429d7bc
Glen Ernstrom <otterbrain@protonmail.com>
2025-06-19 17:59:45 -0400

    working system flake on work PC

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+
+ description = "My first flake!";
+
+ inputs = {
+   nixpkgs.url = "nixpkgs/nixos-unstable";
+   home-manager.url = "github:nix-community/home-manager/master";
+   home-manager.inputs.nixpkgs.follows = "nixpkgs";
+ };
+
+
+
+ outputs = { self, nixpkgs, ... }@inputs:
+    let
+       system = "x86_64-linux";
+       lib = nixpkgs.lib;
+       pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        mendel = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+       }; 
+     };
+     homeConfigurations."ernstrom" = 
+       inputs.home-manager.lib.homeManagerConfiguration {
+       inherit pkgs;
+       modules = [ ./home.nix ];
+       extraSpecialArgs = { inherit inputs; };  
+     };
+    };
+}
